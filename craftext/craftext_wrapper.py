b1d3968dc33fb9e83e628536d322a6b7f31599e6
zoya <zoya.v@ya.ru>
2024-09-12 16:15:39 +0300

    How to make list of jax state? Now it is [state], and culdnt be expand.

--- craftext/craftext_wrapper.py
+++ craftext/craftext_wrapper.py
@@ -3,43 +3,53 @@ from gym import Wrapper
 import random
 import importlib
 import os
+import jax
 from craftext.checkers.base_functions.state_adapter import GameData
 
 def load_scenarios():
     scenarios = {}
     scenarios_dir = "craftext/scenarios"
-    
-    # Загрузка всех сценариев из папки
     for file in os.listdir(scenarios_dir):
-        # print(file)
-        # if file.endswith(".py"):
             scenario_module_name = f"craftext.scenarios.{file}.instructions"
             scenario_module = importlib.import_module(scenario_module_name)
             scenarios.update(scenario_module.instructions)
     
     return scenarios
 
+def load_test_scenarios(test_group='base'):
+    scenarios = {}
+    scenarios_dir = "craftext/scenarios"
+    for file in os.listdir(scenarios_dir):
+            if test_group in file:
+                scenario_module_name = f"craftext.scenarios.{file}.instructions"
+                scenario_module = importlib.import_module(scenario_module_name)
+                scenarios.update(scenario_module.test)
+    return scenarios
+     
 def get_random_instruction_and_checker():
-    # Загружаем все сценарии
-    all_scenarios = load_scenarios()
+    variable_name = "CRAFTEXT_RUN_MODE"
+    value = os.getenv(variable_name)
+    if value == "train":
+        all_scenarios = load_scenarios()
+    else: 
+        variable_name = "TEST_GROUP"
+        value = os.getenv(variable_name)
+        all_scenarios = load_test_scenarios(value)
+
     print("Choose scnario:")
-    for i in range(54, 58):#(len(all_scenarios)):
+    for i in range(len(all_scenarios)):
          print(f"{i}) {list(all_scenarios.keys())[i]}")
-       #  print("Instruction example: ")
-         scenario_key =list(all_scenarios.keys())[i]# random.choice(list(all_scenarios.keys()))
+         scenario_key =list(all_scenarios.keys())[i]
          scenario = all_scenarios[scenario_key]
          print("Instruction example: ", scenario['instruction'])
 
     idx = int(input("Input: "))
-    # Случайно выбираем сценарий
-    random_scenario_key =list(all_scenarios.keys())[idx]# random.choice(list(all_scenarios.keys()))
+    random_scenario_key =list(all_scenarios.keys())[idx]
     random_scenario = all_scenarios[random_scenario_key]
     
-    # Выбираем случайную инструкцию либо из 'instruction', либо из 'instruction_paraphrases'
     instructions = [random_scenario['instruction']] + random_scenario['instruction_paraphrases']
-    random_instruction = instructions[0]#random.choice(instructions)
+    random_instruction = instructions[0]
     keys = [0,0]
-    # Возвращаем выбранную инструкцию и соответствующую check_lambda
     return keys, random_instruction, random_scenario['check_lambda']
 
 
@@ -68,13 +78,16 @@ class InstructionWrapper(Wrapper):
     def step(self,  _rng, env_state, action, env_params):
         # Выполняем шаг основной среды
         obs, state, reward, done, info = self.env.step( _rng, env_state, action, env_params)
-        self.game_data.append(state)
-        self.actions.append(action)
+
+    #    # print("Right state: ", state.inventory)
+    #     self.game_data = jax.numpy.array(self.game_data + [state])
+    #     self.actions = jax.numpy.array(self.actions + [action])
+
         # Добавляем инструкцию к наблюдению
         obs_with_instruction = [obs,self.instruction_keys]
-       # print(self.current_instruction)
-        done = self.checker(GameData(self.game_data,self.actions))
-       # print(done)
+
+        done = self.checker(GameData(state, action))
+
         return obs_with_instruction, state, reward, done, info
 
 if __name__=="__main__":
