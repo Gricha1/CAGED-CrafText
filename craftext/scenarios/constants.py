7b68bd8963f083864e15e40f59e5cedce90267d0
ZoyaV <zoya.v@ya.ru>
2024-10-13 18:10:09 +0300

    add constants for inventar

--- craftext/scenarios/constants.py
+++ craftext/scenarios/constants.py
@@ -1,5 +1,21 @@
 from enum import Enum
 
+
+class InventoryItems(Enum):
+    WOOD = 0
+    STONE = 1
+    COAL = 2
+    IRON = 3
+    DIAMOND = 4
+    SAPLING = 5
+    WOOD_PICKAXE = 6
+    STONE_PICKAXE = 7
+    IRON_PICKAXE = 8
+    WOOD_SWORD = 9
+    STONE_SWORD = 10
+    IRON_SWORD = 11
+ 
+
 class BlockType(Enum):
     INVALID = 0
     OUT_OF_BOUNDS = 1
