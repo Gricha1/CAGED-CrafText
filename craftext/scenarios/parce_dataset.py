2d8df9fc3dca04db906ab7d7535edff076bec436
zoya <zoya.v@ya.ru>
2025-01-22 13:13:09 +0000

    add chuncs return

--- craftext/scenarios/parce_dataset.py
+++ craftext/scenarios/parce_dataset.py
@@ -17,23 +17,30 @@ def parse_instructions(file_name_txt: str) -> List[str]:
     # Parse each chunk into a dictionary using eval
 
     instructions_list = []
+    correct_chunks = []
     for chunk in chunks:
         try:
             if chunk.strip():
                 c = eval(chunk.strip()) 
                 instructions_list.append(c)
+                correct_chunks.append(chunk)
         except Exception as e:
             pass
 
-    return instructions_list
+    return instructions_list,correct_chunks
 
-def parse_instructions_from_folder(folder_path):
+def parse_instructions_from_folder(folder_path, return_chunks=False):
     instructions_list = []
+    correct_chunks = []
     print(os.listdir(folder_path))
     for file_name in os.listdir(folder_path):
         if file_name.endswith('.txt'):
             file_path = os.path.join(folder_path, file_name)
-            instructions_list.extend(parse_instructions(file_path))
+            instructions_list_, correct_chunk = parse_instructions(file_path)
+            instructions_list.extend(instructions_list_)
+            correct_chunks.extend(correct_chunk)
+    if return_chunks:
+        return instructions_list, correct_chunks
     return instructions_list
 
 def update_previous_dict(previous_dict, folder_path, TASK_NAME='relevant_placement'):
