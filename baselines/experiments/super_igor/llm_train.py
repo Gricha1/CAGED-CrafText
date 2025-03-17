3d00d2fefbcc4e12c38585cee06cc069bcd9ce53
volovikova <zoya.v@ya.ru>
2025-03-17 11:28:22 +0300

    add ll, trainning on expert data

--- baselines/experiments/super_igor/llm_train.py
+++ baselines/experiments/super_igor/llm_train.py
@@ -6,7 +6,31 @@ from baselines.experiments.super_igor.encoder import QwenEncodeModel
 from baselines.experiments.super_igor.super_dataset import SuperDataset
 import wandb
 import yaml
+import json
+from baselines.paths import set_os_environ
+from datasets import Dataset
+from baselines.experiments.super_igor.prompts import promt_instruction
 
+def prepare_external_dataset(dataset_path, eos_token=None):
+        prompts = []
+        answers = []
+        with open(dataset_path, "r") as f:
+            data = json.load(f)
+        for instruction in data.keys():
+            plans_options = data[instruction]
+            for i in range(len(plans_options)):
+                    prompts.append(promt_instruction(instruction))
+                    answers.append(plans_options[i]+"\n Finish!")
+
+        text = SuperDataset.formatting(prompts, answers, eos_token)
+    
+        data_dict = {
+        "text": text,
+            }
+       
+        full_dataset = Dataset.from_dict(data_dict) 
+        return full_dataset, data_dict, data_dict
+    
 def load_config(config_path):
     with open(config_path, 'r') as file:
         return yaml.safe_load(file)
@@ -19,34 +43,45 @@ def get_latest_folder(path='./results'):
     latest_folder = max(folders, key=os.path.getctime)
     return latest_folder
 
+
 def train_llm(dataset, 
               previos_checkpoint_dir="Qwen/Qwen2.5-3B-Instruct", 
-              ouput_dir='./results'):
+              ouput_dir='./results', use_external=False):
     
     llm_train_config = load_config('llama_training.yaml')
     llm_train_config['training_args']['output_dir'] = ouput_dir
     encoder = QwenEncodeModel(previos_checkpoint_dir)
 
     eos_token = encoder.tokenizer.eos_token
-    llm_dataset, data_dict, bad_data_dict = dataset.llm_dataset(eos_token=eos_token)
+    if use_external: 
+        llm_dataset, data_dict, bad_data_dict = dataset
+    else:
+        llm_dataset, data_dict, bad_data_dict = dataset.llm_dataset(eos_token=eos_token)
 
     encoder.train(llm_dataset, bad_data_dict, llm_train_config, data_dict)
 
     return get_latest_folder(ouput_dir)
 
 
-def main(dataset_name, llm_name, output_dir):
-    dataset = SuperDataset.load_from_json(dataset_name)    
+def main(dataset_name, external_dataset, llm_name, output_dir):
+    use_external = False    
+    if external_dataset is None:
+        dataset = SuperDataset.load_from_json(dataset_name)
+    else:
+        dataset = prepare_external_dataset(external_dataset)
+        use_external = True
     new_llm_path = train_llm(dataset=dataset, 
                 previos_checkpoint_dir=llm_name, 
-                ouput_dir=output_dir)
+                ouput_dir=output_dir, use_external=use_external)
     return new_llm_path
 
 if __name__=="__main__":
+    set_os_environ()
     wandb.init()
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset_name", default=None, type=str)
+    parser.add_argument("--external_dataset", default=None, type=str)
     parser.add_argument("--llm_name", default=None, type=str)
     parser.add_argument("--output_dir", default=None, type=str)
     args, rest_args = parser.parse_known_args(sys.argv[1:])
-    main(args.dataset_name, args.llm_name, args.output_dir)
+    main(args.dataset_name, args.external_dataset, args.llm_name, args.output_dir)
