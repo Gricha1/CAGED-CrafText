1e1b788aeef0e75dbd84f99809d3592cef9a0c92
volovikova <zoya.v@ya.ru>
2025-03-17 11:28:36 +0300

    add checkpointing

--- baselines/experiments/super_igor/manager.py
+++ baselines/experiments/super_igor/manager.py
@@ -1,4 +1,6 @@
 import subprocess
+from datetime import datetime
+
 import numpy as np
 import wandb
 import os
@@ -43,19 +45,24 @@ def run_policy_train(craftext_settings, env_name,
     
 
 
-def run_policy_inference(llm_name, dataset_name, experiment_name, 
-                         craftext_settings,num_return_sequences='5'):
+def run_policy_inference(llm_name, dataset_name, save_dataset_name, experiment_name, plan_with_llm, 
+                         craftext_settings,num_return_sequences='5', augment="False"):
     args = [
             "python", "policy_inference.py", 
             "--experiment_name", experiment_name,
             "--craftext_settings", craftext_settings,
-            "--num_envs", "512",  
+            "--num_envs", "1024",  
+            "--plan_with_llm", str(plan_with_llm),
             "--inference", "True",  
             "--llm_path", llm_name,
+            "--augment", str(augment),
             "--dataset_path", dataset_name,
+            "--save_dataset_path", save_dataset_name,
             "--num_return_sequences", num_return_sequences,
         ]
 
+    print(args)
+    #exit()
     try:
         subprocess.run(args, check=True)
     except subprocess.CalledProcessError as e:
@@ -113,85 +120,121 @@ if __name__=="__main__":
     name = fake.first_name()
     surname = fake.last_name()
 
-    llm_name = "Qwen/Qwen2.5-3B-Instruct" 
-    experiment_tag = "simple_achivements_one_test"
+    llm_name =  "./pretrained_plan_llm/3_" #""./external_experiments/_9"#"Qwen/Qwen2.5-3B-Instruct" 
+    
+    craftext_settings = "SI_simplified_set"
     use_llm_tuning = True
+    start_from_checkpoint= True
+    validate = False
     
-    experiment_name = f"super_experiments/{experiment_tag}_llmt_{str(use_llm_tuning)}_{name}_{surname}"
-    temp_path = f"{experiment_name}/temp_dataset"
-    craftext_settings = "simple_achivments"
-    rl_experiment_path = "None"
-    super_dataset_name = "None"
+    rl_done = 0
+    inference_done = 0
+    llm_done = 0
     
-    os.makedirs(temp_path, exist_ok=True)
-
-    for j in range(5):
-        run_policy_train(
-            craftext_settings=craftext_settings,
-            env_name="Craftax-Classic-Pixels-v1-Text",
-            llm_path=llm_name,
-            super_dataset=super_dataset_name,
-            num_envs=1024,
-            start_checkpoint_path=rl_experiment_path,
-            experiment_name=experiment_name,
-            encode_form_name="EMBED_CLS_FOR_SPLITS",
-            total_timesteps=50000000#0
-        )
+    rl_skip = 1
+    inference_skip = 2
+    llm_skip = 1
+    
+    if start_from_checkpoint:
         
+        experiment_name = "super_experiments/simple_achivements_one_test_llmt_True_Lawrence_Gibbs_20250311_082123"
+        temp_path = f"{experiment_name}/temp_dataset"
+        dataset_name = f"{temp_path}/super_dataset{0}_{0}.json"
+    else:
+        current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+        experiment_tag = "simple_achivements_one_test"
+        experiment_name = f"super_experiments/{experiment_tag}_llmt_{str(use_llm_tuning)}_{name}_{surname}_{current_time}"
+        temp_path = f"{experiment_name}/temp_dataset"
+        rl_experiment_path = "None"
+        super_dataset_name = "None"
+        os.makedirs(temp_path, exist_ok=True)
+        
+    for j in range(5):
+        
+        if rl_skip<= rl_done:
+            dataset_name = f"{temp_path}/super_dataset{j}_{0}.json"
+            run_policy_train(
+                craftext_settings=craftext_settings,
+                env_name="Craftax-Classic-Pixels-v1-Text",
+                llm_path=llm_name,
+                super_dataset=dataset_name,
+                num_envs=1024,
+                start_checkpoint_path=rl_experiment_path,
+                experiment_name=experiment_name,
+                encode_form_name="EMBED_CLS_FOR_SPLITS",
+                total_timesteps=250000000
+            )
+        else:
+            print("SKIP RL TRAINING!")
+       # exit()
         rl_experiment_path = get_rl_checkpoint_path(experiment_name)
         rl_experiment_name = get_rl_experiment_name(rl_experiment_path)
-
-        print("="*80)
-        print()
-        print(rl_experiment_path)
-        print(rl_experiment_name)
-        print()
-        print("="*80)
-        
-       # rl_experiment_name='run-20241213_171349-clql9lvy'
+        rl_done += 1
+ 
         for i in range(0,4):
-            dataset_name = f"{temp_path}/super_dataset{j}_{i}.json"
+            dataset_name = f"{temp_path}/super_dataset{j}_{i}.json"   
             output_dir = f'./{experiment_name}/llm_checkpoints/mix_text_cycle_{j}_{i}'
-            
-            # Validation on train with new LLM and SuperDataset generation
-            run_policy_inference(llm_name,
-                                 dataset_name, 
-                                 experiment_name=rl_experiment_name,
-                                 craftext_settings=craftext_settings,
-                                 num_return_sequences='20')
-            log_validation(dataset_name, context="train_dataset")
+            plan_with_llm =   i>0 #Generate new plans after LLM training
+            if inference_skip <= inference_done:
+                augment = 1 if i<1 else 0
+                save_dataset_path = dataset_name
+                # Validation on train with new LLM and SuperDataset generation
+                llm_checkpoint = llm_name+"/1_"
+                run_policy_inference(llm_checkpoint,
+                                    dataset_name, 
+                                    save_dataset_path,
+                                    experiment_name=rl_experiment_name,
+                                    plan_with_llm=plan_with_llm,
+                                    craftext_settings=craftext_settings,
+                                    augment=augment,
+                                    num_return_sequences='20')
+                log_validation(dataset_name, context="train_dataset")
+                #exit()
+                save_dataset_path = f"{temp_path}/train_{j}_{i}.json"
+                run_policy_inference(llm_checkpoint, save_dataset_path, save_dataset_path,
+                                        plan_with_llm=True,
+                                        experiment_name=rl_experiment_name,
+                                        craftext_settings=craftext_settings, 
+                                        augment=0,
+                                        num_return_sequences='1')
+                log_validation(save_dataset_path, context="train_1")
+            else:
+                print("SKIP RL INFERECNE!")
+        
+            inference_done += 1
 
+           
             
             if i>0:
                 # Merge datasets
                 old_data_path = f"{temp_path}/super_dataset{j}_{i-1}.json"
                 merger_last_datasets(old_data_path, dataset_name) 
                 #
-                
-                train_results_path = f"{temp_path}/train_{j}_{i}.json"
-                run_policy_inference(llm_name, train_results_path,
-                                    experiment_name=rl_experiment_name,
-                                    craftext_settings=craftext_settings, 
-                                    num_return_sequences='1')
-                log_validation(train_results_path, context="train_1")
-
                 # Validation with RL on parafrases and new goals
-                # test_parafeases_results_path = f"{temp_path}/test_parafeases{j}_{i}.json"
-                # run_policy_inference(llm_name, test_parafeases_results_path,
-                #                     experiment_name=rl_experiment_name,
-                #                     craftext_settings=craftext_settings+"_test_parafrases", 
-                #                     num_return_sequences='1')
-                # log_validation(test_parafeases_results_path, context="test_parafeases")
+                if validate:
+                    test_parafeases_results_path = f"{temp_path}/test_parafeases{j}_{i}.json"
+                    run_policy_inference(llm_name, test_parafeases_results_path,
+                                        experiment_name=rl_experiment_name,
+                                        plan_with_llm=plan_with_llm,
+                                        craftext_settings=craftext_settings+"_test_parafrases", 
+                                        num_return_sequences='1')
+                    log_validation(test_parafeases_results_path, context="test_parafeases")
 
-                # test_new_obj_results_path = f"{temp_path}/test_new_obj{j}_{i}.json"
-                # run_policy_inference(llm_name, test_new_obj_results_path, 
-                #                     experiment_name=rl_experiment_name,
-                #                     craftext_settings=craftext_settings+"_test_other_params", 
-                #                     num_return_sequences='1')
-                # log_validation(test_new_obj_results_path, context="test_new_obj")
+                    test_new_obj_results_path = f"{temp_path}/test_new_obj{j}_{i}.json"
+                    run_policy_inference(llm_name, test_new_obj_results_path, 
+                                        experiment_name=rl_experiment_name,
+                                        plan_with_llm=plan_with_llm,
+                                        craftext_settings=craftext_settings+"_test_other_params", 
+                                        num_return_sequences='1')
+                    log_validation(test_new_obj_results_path, context="test_new_obj")
 
                 # Train LLM
             if use_llm_tuning:
-                run_llm_train(dataset_name, llm_name, output_dir)
+                if llm_skip <= llm_done:
+                    llm_checkpoint = llm_name+"/1_"
+                    run_llm_train(dataset_name, llm_checkpoint, output_dir)
+                else:
+                    print("SKIP LLM TRAIN!")
                 llm_name = output_dir
+                llm_done += 1
         super_dataset_name = dataset_name
