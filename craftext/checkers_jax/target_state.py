3cf5e9a57eaf4cd3b54ce155b7f16eba7e13a046
dmitriy-by-lemonade <dim4ik-super-klass@mail.ru>
2025-03-28 18:49:24 +0800

    update target state, add other States for Localization and Location Placing

--- craftext/checkers_jax/target_state.py
+++ craftext/checkers_jax/target_state.py
@@ -57,15 +57,11 @@ class UnifiedPatternState:
 class TargetState:
     achievements: Achievements = struct.field(default_factory=Achievements)
     building_line: BuildLineState =  struct.field(default_factory=BuildLineState)
-
     building_square: BuildSquareState =  struct.field(default_factory=BuildSquareState)
-
     building_star: BuildStarState =  struct.field(default_factory=BuildStarState)
-
     conditional_placing: ConditionalPlacingState = struct.field(default_factory=ConditionalPlacingState)
     Localiza_placing: LocalizaPlacingState = struct.field(default_factory=LocalizaPlacingState)
     time_placement: TimeCosntrainedPlacmentState = struct.field(default_factory=TimeCosntrainedPlacmentState)
-
     unified_pattern_state: UnifiedPatternState = struct.field(default_factory=UnifiedPatternState)
 
     
