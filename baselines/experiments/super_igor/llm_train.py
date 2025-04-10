ccd06ea5767c1702e16173d280f08a8f4cf66c72
volovikova <zoya.v@ya.ru>
2025-04-10 13:15:28 +0300

    move llm trainer into new code version with planer

--- baselines/experiments/super_igor/llm_train.py
+++ baselines/experiments/super_igor/llm_train.py
@@ -2,14 +2,18 @@ import sys
 import os
 import argparse
 import numpy as np
-from baselines.experiments.super_igor.encoder import QwenEncodeModel
-from baselines.experiments.super_igor.super_dataset import SuperDataset
 import wandb
 import yaml
 import json
-from baselines.paths import set_os_environ
+
+from transformers import AutoTokenizer 
 from datasets import Dataset
-from baselines.experiments.super_igor.prompts import promt_instruction
+
+from baselines.experiments.super_igor.craftext_wrappers.old_encode_code import QwenEncodeModel
+from baselines.experiments.super_igor.craftext_wrappers.encoder_trainer import train
+from baselines.experiments.super_igor.super_dataset import SuperDataset
+from baselines.paths import set_os_environ
+from baselines.experiments.super_igor.prompts import promt_instruction, PlanExtractor
 
 def prepare_external_dataset(dataset_path, eos_token=None):
         prompts = []
@@ -45,34 +49,42 @@ def get_latest_folder(path='./results'):
 
 
 def train_llm(dataset, 
+              plans_type,
+              base_model_name="Qwen/Qwen2.5-3B-Instruct",
               previos_checkpoint_dir="Qwen/Qwen2.5-3B-Instruct", 
               ouput_dir='./results', use_external=False):
     
     llm_train_config = load_config('llama_training.yaml')
     llm_train_config['training_args']['output_dir'] = ouput_dir
-    encoder = QwenEncodeModel(previos_checkpoint_dir)
-
-    eos_token = encoder.tokenizer.eos_token
+   
+    tokenizer = AutoTokenizer.from_pretrained(base_model_name, 
+                                              trust_remote_code=True)
+    eos_token = tokenizer.eos_token
     if use_external: 
         llm_dataset, data_dict, bad_data_dict = dataset
     else:
-        llm_dataset, data_dict, bad_data_dict = dataset.llm_dataset(eos_token=eos_token)
-
-    encoder.train(llm_dataset, bad_data_dict, llm_train_config, data_dict)
-
+        llm_dataset, data_dict, bad_data_dict = dataset.llm_dataset(eos_token=eos_token,plans_type=plans_type)
+    
+    train(dataset=llm_dataset, data_for_inference=data_dict, plans_type=plans_type,
+          train_config=llm_train_config,
+          plan_model_name=previos_checkpoint_dir)
+    
     return get_latest_folder(ouput_dir)
 
 
-def main(dataset_name, external_dataset, llm_name, output_dir):
+def main(dataset_name, base_model_name, external_dataset, llm_name, plans_type, output_dir):
     use_external = False    
+
     if external_dataset is None:
         dataset = SuperDataset.load_from_json(dataset_name)
     else:
         dataset = prepare_external_dataset(external_dataset)
         use_external = True
     new_llm_path = train_llm(dataset=dataset, 
-                previos_checkpoint_dir=llm_name, 
-                ouput_dir=output_dir, use_external=use_external)
+                            base_model_name=base_model_name,
+                            previos_checkpoint_dir=llm_name, 
+                            ouput_dir=output_dir, use_external=use_external,
+                            plans_type=plans_type)
     return new_llm_path
 
 if __name__=="__main__":
@@ -83,5 +95,9 @@ if __name__=="__main__":
     parser.add_argument("--external_dataset", default=None, type=str)
     parser.add_argument("--llm_name", default=None, type=str)
     parser.add_argument("--output_dir", default=None, type=str)
+    parser.add_argument("--base_model_name", default="Qwen/Qwen2.5-3B-Instruct", type=str)
+    parser.add_argument("--plans_type", default=None, type=int)
     args, rest_args = parser.parse_known_args(sys.argv[1:])
-    main(args.dataset_name, args.external_dataset, args.llm_name, args.output_dir)
+    main(args.dataset_name, args.base_model_name,
+         args.external_dataset, args.llm_name, 
+         args.plans_type, args.output_dir)
