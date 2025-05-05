a2e9e8f510f4e78a94a48dd5d6b5a65d88ae2bac
dmitriy-by-lemonade <dim4ik-super-klass@mail.ru>
2025-05-05 23:37:56 +0800

    monoenv init

--- baselines/experiments/utils.py
+++
@@ -1,7 +0,0 @@
-def magic_print(sym="!", text=""):
-    print(sym * 80)
-    print()
-    print(text)
-    print()
-    print(sym * 80)
-
--- baselines/paths.py
+++
@@ -1,4 +0,0 @@
-import os
-
-def set_os_environ():   
-    os.environ["HF_HOME"] = "/home/volovikova/CrafText/baselines/base_models/"
