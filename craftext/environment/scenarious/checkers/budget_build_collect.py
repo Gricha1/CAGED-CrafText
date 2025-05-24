a33cdc9f2b6c8a470a065b8f83acec15808faa6c
dmitriy-by-lemonade <dim4ik-super-klass@mail.ru>
2025-05-25 06:53:52 +0800

    improve building budget checker

--- craftext/environment/scenarious/checkers/budget_build_collect.py
+++ craftext/environment/scenarious/checkers/budget_build_collect.py
@@ -20,10 +20,46 @@ from craftext.environment.scenarious.checkers.squeres import (
 from craftext.environment.scenarious.checkers.target_state_cmdp_build_budget_collect import BuildBudgetState, CMDPTargetState
 
 def checker_budget_build_collect(game_data: Union[GameDataClassic, GameData],  target_state: BuildBudgetState) -> jax.Array:
-    raise NotImplementedError("checker_budget_build_collect is not implemented yet")
+    # raise NotImplementedError("checker_budget_build_collect is not implemented yet")
     block_type  = target_state.block_type
+
+    def get_item(index, inventory):
+        return lax.switch(index,
+            [
+                lambda: inventory.wood,
+                lambda: inventory.stone,
+                lambda: inventory.coal,
+                lambda: inventory.iron,
+                lambda: inventory.diamond,
+                lambda: inventory.sapling,
+                lambda: inventory.wood_pickaxe,
+                lambda: inventory.stone_pickaxe,
+                lambda: inventory.iron_pickaxe,
+                lambda: inventory.wood_sword,
+                lambda: inventory.stone_sword,
+                lambda: inventory.iron_sword,
+                
+                # lambda: lax.switch(inventory.inventory, [lambda: inventory.pickaxe, lambda: inventory.wood_pickaxe]), 
+                # lambda: lax.switch(inventory.inventory, [lambda: inventory.sword, lambda: inventory.stone_pickaxe]),
+                # lambda: lax.switch(inventory.inventory, [lambda: inventory.bow,   lambda: inventory.iron_pickaxe]), 
+                # lambda: lax.switch(inventory.inventory, [lambda: inventory.arrows, lambda: inventory.wood_sword]),
+                # lambda: lax.switch(inventory.inventory, [lambda: inventory.armour, lambda: inventory.stone_sword]),
+                # lambda: lax.switch(inventory.inventory, [lambda: inventory.torches, lambda: inventory.iron_sword]),
+            
+                # lambda: inventory.ruby,
+                # lambda: inventory.sapphire,
+                
+                # lambda: inventory.diamond, #lambda: inventory.potions
+                # lambda: inventory.diamond, #lambda: inventory.books
+            ]
+        )
     
-    return is_on_block(game_data, block_type)
+    
+    
+    
+    curr = get_item(block_type, game_data.states[0].inventory)
+    prev = get_item(block_type, game_data.states[1].inventory)
+    return curr - prev > 0
 
 def is_on_block(gd: Union[GameDataClassic, GameData], block_type):
     x, y = gd.states[0].variables.player_position
