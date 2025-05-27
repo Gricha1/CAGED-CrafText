a74970486303c5ee67251ae8037defd966c67a70
dmitriy-by-lemonade <dim4ik-super-klass@mail.ru>
2025-05-27 18:49:04 +0800

    cmdp: update dataset, add target state

--- craftext/dataset/scenarious/jax_achievements_safe_constraint_budget_drink/instructions.py
+++ craftext/dataset/scenarious/jax_achievements_safe_constraint_budget_drink/instructions.py
@@ -1,9 +1,11 @@
 
 from craftext.environment.scenarious.checkers.target_state import Achievements
 from craftext.environment.craftext_constants import Achievement, Scenarios, AchievementState, BlockType
-from craftext.environment.scenarious.checkers.target_state_cmdp import StepOnBlock, CMDPTargetState
+# from craftext.environment.scenarious.checkers.target_state_cmdp import StepOnBlock, CMDPTargetState
+from craftext.environment.scenarious.checkers.target_state_cmdp_budget_drink_level import CMDPTargetState, DrinkLevelState
 
-def create_target_state(required=[], forbidden=[], block_type:int=0):
+
+def create_target_state(required=[], forbidden=[], level:int=0):
     base_vector = [AchievementState.NOT_MATTER for i in range(Achievement.MAKE_IRON_SWORD + 1)]
     for i in range(len(base_vector)):
         if i in required:
@@ -11,8 +13,9 @@ def create_target_state(required=[], forbidden=[], block_type:int=0):
         elif i in forbidden:
             base_vector[i] = AchievementState.AVOID_TO_ACHIEVE
     target_achievements = Achievements(achievement_mask=tuple(base_vector))
-    step_on_block = StepOnBlock(block_type=block_type)
-    return CMDPTargetState(achievements=target_achievements, step_on_block=step_on_block)
+    # step_on_block = StepOnBlock(block_type=block_type)
+    level = DrinkLevelState(level=level)
+    return CMDPTargetState(achievements=target_achievements, drink_level_state=DrinkLevelState)
 
 
 easy = { 
--- craftext/dataset/scenarious/jax_achievements_safe_constraint_budget_energy/instructions.py
+++ craftext/dataset/scenarious/jax_achievements_safe_constraint_budget_energy/instructions.py
@@ -1,9 +1,10 @@
 
 from craftext.environment.scenarious.checkers.target_state import Achievements
 from craftext.environment.craftext_constants import Achievement, Scenarios, AchievementState, BlockType
-from craftext.environment.scenarious.checkers.target_state_cmdp import StepOnBlock, CMDPTargetState
+from craftext.environment.scenarious.checkers.target_state_cmdp_budget_energy_level import CMDPTargetState, EnergyLevelState
 
-def create_target_state(required=[], forbidden=[], block_type:int=0):
+
+def create_target_state(required=[], forbidden=[], level:int=0):
     base_vector = [AchievementState.NOT_MATTER for i in range(Achievement.MAKE_IRON_SWORD + 1)]
     for i in range(len(base_vector)):
         if i in required:
@@ -11,9 +12,9 @@ def create_target_state(required=[], forbidden=[], block_type:int=0):
         elif i in forbidden:
             base_vector[i] = AchievementState.AVOID_TO_ACHIEVE
     target_achievements = Achievements(achievement_mask=tuple(base_vector))
-    step_on_block = StepOnBlock(block_type=block_type)
-    return CMDPTargetState(achievements=target_achievements, step_on_block=step_on_block)
-
+    # step_on_block = StepOnBlock(block_type=block_type)
+    level = EnergyLevelState(level=level)
+    return CMDPTargetState(achievements=target_achievements, drink_level_state=EnergyLevelState)
 
 easy = { 
   
