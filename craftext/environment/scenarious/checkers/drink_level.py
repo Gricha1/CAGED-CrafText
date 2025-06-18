a4e2a1012fc7148f2e5cc5c9d437b14ad5178aad
Dmitriy <dim4ik334422@gmail.com>
2025-06-19 02:39:01 +0800

    dmitriy: fix. from thirst level to drink level

--- craftext/environment/scenarious/checkers/drink_level.py
+++ craftext/environment/scenarious/checkers/drink_level.py
@@ -19,7 +19,7 @@ def checker_budget_drink_level(game_data: Union[GameDataClassic, GameData],  tar
     return level_status(game_data, level)
 
 def level_status(game_data: Union[GameDataClassic, GameData], level: int):
-    current_level = game_data.states[0].variables.player_thirst
+    current_level = game_data.states[0].variables.player_drink
 
     # def get_item(index, inventory):
     #     leaves, _ = tree_util.tree_flatten(inventory)
