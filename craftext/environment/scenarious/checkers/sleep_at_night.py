8ca26e32967632a70487b6e045a09c50bb44ced2
Dmitriy <dim4ik334422@gmail.com>
2025-06-18 23:51:21 +0800

    dmitriy: checker for sleep_at_noght

--- craftext/environment/scenarious/checkers/sleep_at_night.py
+++ craftext/environment/scenarious/checkers/sleep_at_night.py
@@ -2,40 +2,40 @@ import jax
 
 from jax import (
     numpy as jnp,
-    lax
 )
-from jax import tree_util
 
 from typing import Union
 from craftext.environment.states.state import GameData
 from craftext.environment.states.state_classic import GameDataClassic
 
-from craftext.environment.scenarious.checkers.target_state_light_level import LightLevelState
-from craftext.environment.craftext_constants import BlockType
+from craftext.environment.scenarious.checkers.target_state_cmdp_night_day_light import LightLevelState
 
-from craftax.craftax.constants import Action as ActionExtend
-from craftax.craftax_classic.constants import Action as ActionClassic
+
 from typing import Union
 
-# class Action(ActionExtend, ActionClassic):
-#     pass
 
-def checker_budget_drink_level(game_data: Union[GameDataClassic, GameData],  target_state: LightLevelState) -> jax.Array:
-    # raise NotImplementedError("checker_budget_build_collect is not implemented yet")
-    light_level = target_state.level
-    return is_sleep_at_night(game_data, light_level)
 
-def is_sleep_at_night(game_data: Union[GameDataClassic, GameData], light_level):
+def checker_sleep_at_night(game_data: Union[GameDataClassic, GameData],  night_constraint_level: LightLevelState, day_constraint_level: LightLevelState) -> jax.Array:
+    night = night_constraint_level.level
+    day = day_constraint_level.level
+    return is_sleep_at_night(game_data, night, day)
+
+def is_sleep_at_night(game_data: Union[GameDataClassic, GameData], night, day):
     is_sleep = game_data.states[0].variables.is_sleeping               # jax.Array с целочисленным кодом действия
     light_level = game_data.states[0].variables.light_level  # jax.Array с уровнем освещённости
 
-    # Порог из целевого состояния:
-    threshold = light_level                     # число или jax.Array
+
+    #night
+    threshold = night
+    is_dark_enough = light_level < threshold
+    
+
 
 
     # Проверяем условия:
-    is_dark_enough = light_level <= threshold
+    threshold = day
+    is_light_ebove = light_level > threshold
 
-    return jnp.logical_and(is_dark_enough, is_sleep)
+    return jnp.logical_not(jnp.logical_and(is_dark_enough, is_sleep)) | (jnp.logical_and(is_light_ebove, is_sleep))
     
 
