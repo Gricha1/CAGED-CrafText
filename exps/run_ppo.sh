3c0d02b38db307108c203d72b65e402b1f56a35f
Gricha1 <trololoeeev@mail.ru>
2025-09-03 17:55:19 +0300

    gregory: add exps folder

---
+++ exps/run_ppo.sh
@@ -0,0 +1,4 @@
+cd ..
+cd baselines
+
+python cmdp_ppo_with_instruction.py --craftext_settings achievements_easy_safe_budget_drink --env_name="Craftax-Classic-Pixels-v1-Text" --num_envs=512
---
+++ exps/run_ppo_lag.sh
@@ -0,0 +1,4 @@
+cd ..
+cd baselines
+
+python ppo_lag_with_instruction.py --craftext_settings achievements_easy_safe_budget_drink --env_name="Craftax-Classic-Pixels-v1-Text" --num_envs=512
