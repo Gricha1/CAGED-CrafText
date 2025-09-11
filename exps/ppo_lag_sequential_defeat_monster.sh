fff78512bb768005765c4911eeda52afabbfff51
Gricha1 <trololoeeev@mail.ru>
2025-09-11 23:53:22 +0300

    gregory: fix sequential defeat monsters dataset + exps

---
+++ exps/ppo_lag_sequential_defeat_monster.sh
@@ -0,0 +1,5 @@
+cd ..
+cd baselines
+export COMET_API_KEY="3OfuYHwcRgIwG7DzgzJ190igY"
+
+python ppo_lag_with_instruction.py --craftext_settings achievements_safe_sequential_defeat_monster --env_name="Craftax-Classic-Pixels-v1-Text" --num_envs=512
