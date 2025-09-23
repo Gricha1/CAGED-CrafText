db7719c45b157e4bdad249188c86e585a26da083
ggorbov <ggorbov@login-2.hpc.frccsc.ru>
2025-09-23 16:29:57 +0300

    gregory: fix sequential all training

--- craftext/environment/craftext_wrapper_cmdp.py
+++ craftext/environment/craftext_wrapper_cmdp.py
@@ -121,14 +121,20 @@ class CMDPInstructionWrapper(InstructionWrapper):
             cost = checker_away_from_monsters_when_hp_low(game_data_vector, ts.hp_level_state).astype(float)
         elif self.config_name == "achievements_safe_sequential_all":
             task_type = ts.hp_level_state.level
-            if task_type == -1:
-                # You cannot sleep when monsters are nearby
-                cost = checker_dont_sleep_near_monsters(game_data_vector).astype(float)
-            elif task_type == -2:
-                # You must not attack any monster w/o sword
-                cost = checker_monster_is_attacked_without_sword(game_data_vector).astype(float)
-            else:
-                cost = checker_away_from_monsters_when_hp_low(game_data_vector, ts.hp_level_state).astype(float)
+            # Создаем индекс для switch
+            index = jnp.where(task_type == -1, 0, jnp.where(task_type == -2, 1, 2))
+            
+            cost = jax.lax.switch(
+                index,
+                [
+                    # case 0: task_type == -1 You cannot sleep when monsters are nearby
+                    lambda: checker_dont_sleep_near_monsters(game_data_vector).astype(float),
+                    # case 1: task_type == -2 You must not attack any monster w/o sword
+                    lambda: checker_monster_is_attacked_without_sword(game_data_vector).astype(float),
+                    # case 2: otherwise
+                    lambda: checker_away_from_monsters_when_hp_low(game_data_vector, ts.hp_level_state).astype(float)
+                ]
+            )
                 
         else:
             assert 1 == 0, f"unknow config name: {self.config_name}, need assign cost function for this config"
