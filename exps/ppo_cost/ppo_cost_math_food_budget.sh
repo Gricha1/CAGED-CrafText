79b379fe99f6e3d8b955af1e5539406db9303909
Gricha1 <trololoeeev@mail.ru>
2025-10-14 21:25:09 +0300

    gregory: add all ppo cost exps configs

---
+++ exps/ppo_cost/ppo_cost_math_food_budget.sh
@@ -0,0 +1,6 @@
+cd ../..
+cd baselines
+export COMET_API_KEY="3OfuYHwcRgIwG7DzgzJ190igY"
+
+python ppo_cost_with_instruction.py --craftext_settings achievements_safe_math_food_budget --env_name="Craftax-Classic-Pixels-v1-Text" --num_envs=512 \
+                                    --cost_koef 0.5
