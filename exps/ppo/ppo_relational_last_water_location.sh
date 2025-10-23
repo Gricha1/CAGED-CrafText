a8af43cb1425d3ca677cee7838ec06ef398d236d
Gricha1 <trololoeeev@mail.ru>
2025-10-23 16:56:50 +0300

    gregory: add ppo relational last water location

---
+++ exps/ppo/ppo_relational_last_water_location.sh
@@ -0,0 +1,5 @@
+cd ../..
+cd baselines
+export COMET_API_KEY="3OfuYHwcRgIwG7DzgzJ190igY"
+
+python cmdp_ppo_with_instruction.py --craftext_settings achievements_easy_relational_last_water_location --env_name="Craftax-Classic-Pixels-v1-Text" --num_envs=512
